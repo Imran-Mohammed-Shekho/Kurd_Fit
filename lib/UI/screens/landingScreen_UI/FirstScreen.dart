ef77976b0fd2b8c85feee5cc86e6abc1a22e38b9
IMran <aemrancr@gmail.com>
2025-11-22 13:11:21 +0300

    i have updated first screen in landing scree

--- lib/UI/screens/landingScreen_UI/FirstScreen.dart
+++ lib/UI/screens/landingScreen_UI/FirstScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gym/UI/screens/landingScreen_UI/activityLevelScreen.dart';
 import 'package:gym/UI/screens/landingScreen_UI/genderScreen/GenderButtom.dart';
 import 'package:gym/UI/screens/landingScreen_UI/questionScreenControll.dart';
 
@@ -12,6 +13,7 @@ class Introduction extends StatefulWidget {
 class _IntroductionState extends State<Introduction> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xff6157C9),
       body: Stack(
@@ -20,6 +22,7 @@ class _IntroductionState extends State<Introduction> {
             child: Image.asset(
               "assets/images/introduction.png",
               fit: BoxFit.cover,
+              width: size.width,
             ),
           ),
 
@@ -31,6 +34,25 @@ class _IntroductionState extends State<Introduction> {
           //     width: double.infinity,
           //   ),
           // ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: size.height,
+              width: size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xff6157C9),
+                    Color(0xff6157C9),
+                    kwhite.withValues(alpha: 0.3),
+                    kwhite.withValues(alpha: 0.3),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+            ),
+          ),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -52,7 +74,26 @@ class _IntroductionState extends State<Introduction> {
                 ),
                 SizedBox(height: 30),
 
-                buildButtom(ontap: () {}),
+                buildButtom(
+                  ontap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Questionscreencontroll(),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Already Have account ? Login",
+                  style: TextStyle(
+                    color: kwhite,
+                    decoration: TextDecoration.underline,
+                    decorationColor: kwhite,
+                    fontSize: 16,
+                  ),
+                ),
 
                 SizedBox(height: 40),
               ],
